3c0df3e84e845bd4a5b00b608c96bf0e8842dea2
ung <ungureanu.bogdan2007@gmail.com>
2026-04-06 16:38:19 +0300

    added 5.1.3 5.1.5 5.3.1 to lab2

--- Lab2/5.1.5.cpp
+++ Lab2/5.1.5.cpp
@@ -1,12 +1,32 @@
-#include <iostream>
+class Solution {
+public:
+    string removeDuplicates(string s, int k) {
+        stack<pair<char, int>> S;
+        S.push({s[0], 1});
+        for(int i = 1; i < s.size(); ++i) {
 
-using namespace std;
+            while(i < s.size() && !S.empty() && s[i] == S.top().first) {
+                auto x = S.top();
+                S.pop();
 
-string s;
-int k;
+                if(x.second < k - 1) {
+                    S.push({s[i], x.second + 1});
+                }
+                ++i;
+            }
+            if(i < s.size()) S.push({s[i], 1});
+            
+        }
+        string ans;
+        while(!S.empty()) {
+            auto x = S.top();
+            S.pop();
+            for(int i = 0; i < x.second; ++i) ans.push_back(x.first);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
 
-int main()
-{
-    cin >> s;
-    cin >> k;
-}
+};
+
+//https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string-ii/solutions/7796552/sol-by-ung-ki55/
--- Lab2/5.3.1.cpp
+++ Lab2/5.3.1.cpp
@@ -1,14 +1,40 @@
-#include <iostream>
+#include <fstream>
 #include <deque>
 
 using namespace std;
 
-int n, k;
-int a[1001];
+ifstream cin("deque.in");
+ofstream cout("deque.out");
 
-int main()
-{
-    cin >> n >> k;
-    for(int i = 1; i <= n; ++i) cin >> a[i];
+int n, k;
+int a[5000001];
+deque<int> dq;
+
+int main() {
     
+    cin >> n >> k;
+    for(int i = 1; i <= n; ++i) {
+        cin >> a[i];
+    }
+
+    long long sum = 0;
+    
+    for(int i = 1; i <= n; ++i) {
+        
+        while(!dq.empty() && a[dq.back()] >= a[i]) dq.pop_back();
+        dq.push_back(i);
+
+        while(dq.front() <= i - k) dq.pop_front();
+        
+        if(i >= k) {
+            sum += a[dq.front()];
+        }
+
+    }
+
+    cout << sum << '\n';
+
+    return 0;
 }   
+
+// https://www.infoarena.ro/job_detail/3350231
